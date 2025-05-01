d2ab212759d15785386e4d2844e88000fb427ed8
Apurba Koley <apurba@Apurbas-MacBook-Pro.local>
2025-05-01 21:28:45 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const mongoose = require('mongoose');
-const express = require('express')
-var cors = require('cors')
-const port= 5000
-const app = express()
-app.use(express.json())
+const express = require('express');
+var cors = require('cors');
+const port = 5000;
+const app = express();
+app.use(express.json());
 
-app.use(cors())
+// Configure CORS for a specific origin
+const corsOptions = {
+  origin: 'https://wallet-x-frontend.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you need to handle cookies or authorization headers
+  optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+};
 
-app.use(express.urlencoded({extended:true})) // middleware jisse body dikhe
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/transaction',require('./routes/transaction'))
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true })); // middleware jisse body dikhe
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/transaction', require('./routes/transaction'));
+
 main().catch(err => console.log(err));
 
 async function main() {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect('mongodb+srv://apurbaKoley:8oqrAjOaOPdbumcG@cluster0.1ykbjea.mongodb.net/WalletX');
+  mongoose.set("strictQuery", false);
+  await mongoose.connect('mongodb+srv://apurbaKoley:8oqrAjOaOPdbumcG@cluster0.1ykbjea.mongodb.net/WalletX');
 
-    console.log("we are connected")
+  console.log("we are connected");
 }
-app.get("/",(req,res)=> {
-    res.send("Backend working lol")
-})
-app.get('/test',(req,res)=> {
-    res.send("deployment test working")
-})
-app.listen(port,()=> {
-    console.log(`Server running succesfully at https://wallet-x-backend.vercel.app/`)
-})
-module.exports = app
+
+app.get("/", (req, res) => {
+  res.send("Backend working lol");
+});
+
+app.get('/test', (req, res) => {
+  res.send("deployment test working");
+});
+
+app.listen(port, () => {
+  console.log(`Server running successfully at https://wallet-x-backend.vercel.app/`);
+});
+
+module.exports = app;
